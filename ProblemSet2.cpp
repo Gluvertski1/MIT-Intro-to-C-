086540c700f12aca5319be897ccd82fb9faaf74a
Jared <73083139+Gluvertski1@users.noreply.github.com>
2022-05-09 18:59:23 -0400

    Add files via upload

---
+++ ProblemSet2.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+// int sum(const int a, const int b){
+//     return a + b;
+// }
+
+// double sum(const double a, const double b){
+//     return a + b;
+// }
+
+// int sum(const int a, const int b, const int c){
+//     return a + b + c;
+// }
+
+// int sum(const int a, const int b, const int c, const int d){
+//     return a + b + c + d;
+// }
+
+// int sum(const int a, const int b, const int c = 0, const int d = 0){
+//     return a + b + c + d;
+// }
+
+int sum(){
+    int sum = 0;
+    for(int i = 0; i<n; i++)
+    {
+
+    }
+    return sum;
+}
+
+int main(){
+
+    int x = 1;
+    int y = 2;
+
+    double f = 1.0;
+    double g = 2.1;
+
+    cout << sum(3,5,7) << "\n";
+
+    cout << sum(f,g) << "\n";
+
+}
